535834443fa64258bd7de35b468569fcbc2c9d46
Minjae Joe <ussr1285@ajou.ac.kr>
2023-11-28 16:35:06 +0900

    README.md에 프로그램 개요 작성

---
+++ 2/interface.c
@@ -0,0 +1,19 @@
+
+
+void userInputs();
+
+int main(void) {
+    userInputs();
+
+    while(1) {
+		
+    }
+    return 0;
+}
+
+void userInputs(int *something)
+{
+    printf("Please enter : ");
+	scanf("%d", something);
+	return ;
+}
